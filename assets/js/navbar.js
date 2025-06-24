c80fee1bbd397a428f0f65a2ec11e64d3b2b6a94
Rija-noor <rijanoor454@gmail.com>
2025-06-24 10:35:21 -0700

    chnges

---
+++ assets/js/navbar.js
@@ -0,0 +1,30 @@
+document.querySelector("header").innerHTML += `
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand ms-2" href="#">RIJA-NOOR</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-4 mb-2 mb-lg-0 ms-auto p-3">
+        <li class="nav-item">
+          <a class="nav-link active ms-2 me-4" aria-current="page" href="index.html">Home</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link ms-2 me-4" href="about.html">About Me</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link ms-2 me-4" href="resume.html">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ms-2 me-4" href="projects.html">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ms-2 me-4" href="contact.html">Contact Me</a>
+        </li>
+      </ul>
+          </div>
+  </div>
+</nav>
+`
